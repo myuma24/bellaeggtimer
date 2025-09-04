e056dd568b818321b3f1e80b3b27dbd2f3edb299
myuma24 <maggiema951@gmail.com>
2025-09-04 00:09:00 -0700

    sam updates

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const buttons = document.querySelectorAll("button");
 const message = document.getElementById("message");
+const timerMessage = document.getElementById("timer-message");
 const display = document.getElementById("timerDisplay");
+const main = document.getElementById("main");
+const timer = document.getElementById("timer");
 
 let countdown; // global interval ID
 
@@ -31,8 +34,39 @@ function updateDisplay(seconds) {
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        // first, we make sure timer is completely hidden and removed from "document flow"
+        timer.style.display = "none";
+
         const time = parseInt(button.dataset.time);
-        message.textContent = `🍳 Cooking: ${button.textContent}`;
+        timerMessage.textContent = `🍳 Cooking: ${button.textContent}`;
         startTimer(time);
+
+        /* begin section transition animation */
+
+        // first, fade out to opacity: 0
+        const animationTime = 400;
+        main.style.transition = `opacity ${animationTime}ms, transform ${animationTime}ms`;
+
+        setTimeout(() => {
+            main.style.transform = "scale(0.9) translateY(-24px)";
+            main.style.opacity = "0";
+        }, 1000 / 30);
+
+        setTimeout(() => {
+            main.style.display = "none";
+
+            // follow up code to fade in egg timer
+            timer.style.opacity = "0";
+            timer.style.transform = "scale(0.7) translateY(-18px)";
+            timer.style.transition = `opacity ${animationTime}ms, transform ${animationTime}ms`;
+
+            timer.style.display = "block";
+            setTimeout(() => {
+                timer.style.opacity = "1";
+                timer.style.transform = "scale(1) translateY(0px)";
+            }, 1000 / 30);
+        }, animationTime);
     });
 });
+
+
