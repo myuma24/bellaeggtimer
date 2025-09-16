df732c618f724e3c48e04b6118b55bb719711d5e
Samuel Li <samuelli4521@gmail.com>
2025-09-15 19:04:19 -0700

    electron

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { app, BrowserWindow } = require("electron");
+
+const electronReload = require("electron-reload");
+electronReload(__dirname);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("files/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
